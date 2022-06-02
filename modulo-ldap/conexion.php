a6dc59515c65af61270287026ed195a3e1524e85
gsalazar1998 <gsalazar@tpitic.com.mx>
2022-06-02 16:07:52 -0400

    se cambio password airwatch

---
+++ modulo-ldap/conexion.php
@@ -0,0 +1,30 @@
+<?php
+
+/*esta es la clase para hacer la conexion a ldap*/
+class Conexion{
+	var $server_ldap = "ldap.tpitic.com.mx";
+	var $dn = "cn=feria,dc=transportespitic,dc=com";
+	var $password = "sistemaspitic";
+
+	
+	function __contruct(){
+	
+	}
+	
+	function conectarLDAP(){
+		$con = ldap_connect($this->server_ldap);
+		$anon = ldap_bind( $con );
+		if(!$anon){
+			$con=ldap_connect('ldapbackup.tpitic.com.mx');
+		}
+		if ($con) {
+		   $bind=ldap_bind($con, $this->dn, $this->password)or die("Can't bind to server.");
+		   return $con;
+		}else{
+			return null;
+		}
+	}
+	
+	
+} 
+?>
