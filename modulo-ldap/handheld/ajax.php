90b364e055aa709e0d15cc2b31716350116e3d10
gsalazar1998 <gsalazar@tpitic.com.mx>
2022-06-14 15:29:21 -0400

    modulo hand held

--- modulo-ldap/handheld/ajax.php
+++ modulo-ldap/handheld/ajax.php
@@ -1,12 +1,10 @@
 <?php
-$lanip = $_POST['lanip'];
-$lanmac = $_POST['lanmac'];
-$wip = $_POST['wip'];
-$wmac = $_POST['wmac'];
-$nivel = $_POST['nivel'];
-$usuario = $_POST['usuario'];
-$puesto = $_POST['puesto'];
-$oficina = $_POST['oficina'];
+$brand = $_POST['brand'];
+$ip=$_POST['ip'];
+$mac=$_POST['mac'];
+$office=$_POST['office'];
+$serial=$_POST['serial'];
+$tag=$_POST['tag'];
 //echo "este es $extension  tambien  $usuario  y  $oficina"
 ?>
 
@@ -15,15 +13,15 @@ $oficina = $_POST['oficina'];
         <div class="editar formulario">
             <div class="solicitud1">
                 <div>
-                    <label for="usuario">Usuario:</label>
+                    <label for="tag">Tag:</label>
                 </div>
                 <div>
-                    <input class="ext" id="usuario" type="text" name="busuario" readonly value="<?php echo $usuario; ?>">
+                    <input class="ext" id="tag" type="text" name="busuario" readonly value="<?php echo $usuario; ?>">
                 </div>
             </div>
             <div class="solicitud1">
                 <div>
-                    <label for="puesto">Puesto:</label>
+                    <label for="puesto">Marca:</label>
                 </div>
                 <div>
                     <input class="ext" id="puesto" type="text" name="bpuesto" readonly value="<?php echo $puesto; ?>">
@@ -31,16 +29,53 @@ $oficina = $_POST['oficina'];
             </div>
             <div class="solicitud1">
                 <div>
-                    <label for="iplan">Lan ip:</label>
+                    <label for="iplan">Oficina:</label>
                 </div>
                 <div>
-                    <input class="usuario" id="iplan" type="text" name="blanip" pattern="^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)|NO$" value="<?php echo $lanip; ?>">
+                    <select id="oficina" name="boficina" class="ofi">
+                        <option hidden selected><?php echo $oficina; ?></option>
+                        <option value="TRA">Transportes</option>
+                        <option value="VHL">Volvo Hermosillo</option>
+                        <option value="TDI">Tecnologia Diesel</option>
+                        <option value="DO">Direccion de operaciones</option>
+                        <option value="DC">Direccion comercial</option>
+                        <option value="DG">Direccion general</option>
+                        <option value="RH">Recursos humanos</option>
+                        <option value="DAF">Administracion (DAF)</option>
+                        <option value="SIS">Sistemas</option>
+                        <option value="MT1">Monterrey 1</option>
+                        <option value="MER">Merida</option>
+                        <option value="CUL">Culiacan</option>
+                        <option value="MCH">Mochis</option>
+                        <option value="NOG">Nogales</option>
+                        <option value="CCN">Cancun</option>
+                        <option value="MAZ">Mazatlan</option>
+                        <option value="MXL">Mexicali</option>
+                        <option value="PUE">Puebla</option>
+                        <option value="QUE">Queretaro</option>
+                        <option value="TEP">Tepic</option>
+                        <option value="LGT">Leon</option>
+                        <option value="IZT">Iztapalapa</option>
+                        <option value="ZAP">Zapopan</option>
+                        <option value="CHI">Chihuahua</option>
+                        <option value="STA">Santa ana</option>
+                        <option value="TOL">Toluca</option>
+                        <option value="JUA">Juarez</option>
+                        <option value="TPZ">Tepozotlan</option>
+                        <option value="GDL">Guadalajara</option>
+                        <option value="HLO">Hermosillo</option>
+                        <option value="MEX">Mexico</option>
+                        <option value="VIL">Villahermosa</option>
+                        <option value="TIJ">Tijuana</option>
+                        <option value="COB">Ciudad obregon</option>
+                        <option value="MTY">Monterrey</option>
+                        </select>
                 </div>
 
             </div>
             <div class="solicitud1">
                 <div>
-                    <label for="wip">Wifi ip:</label>
+                    <label for="wip">Serial:</label>
                 </div>
                 <div>
                     <input class="usuario" id="wip" type="text" name="bwip" pattern="^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)|NO$" value="<?php echo $wip; ?>">
@@ -49,7 +84,7 @@ $oficina = $_POST['oficina'];
             </div>
             <div class="solicitud1">
                 <div>
-                    <label for="nivel">Niveles de red:</label>
+                    <label for="nivel">Ip:</label>
                 </div>
                 <div>
                     <select id="nivel" name="bnivel" class="ofi" required>
@@ -64,7 +99,7 @@ $oficina = $_POST['oficina'];
             </div>
             <div class="solicitud1">
                 <div>
-                    <label for="oficina">oficina:</label>
+                    <label for="oficina">Mac:</label>
                 </div>
                 <div>
                     <select id="oficina" name="boficina" class="ofi">
