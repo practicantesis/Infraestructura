14760669c5ecc7a5bbf4f63809f76a15bcea3590
gsalazar1998 <gsalazar@tpitic.com.mx>
2022-07-12 18:06:10 -0400

    creacion de modulo niveles de red

--- modulo-ldap/niveles/3/index.php
+++ modulo-ldap/niveles/3/index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/estilos2.css">
-    <link rel="shortcut icon" href="./img/ipIcon.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/estilos2.css">
+    <link rel="shortcut icon" href="../../img/ipIcon.png" type="image/x-icon">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
