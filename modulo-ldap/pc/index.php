dab0a1a4291aa852e213361c501aa1b0bf866bda
gsalazar1998 <gsalazar@tpitic.com.mx>
2022-07-22 16:35:30 -0400

    agregar icono de pagina

--- modulo-ldap/pc/index.php
+++ modulo-ldap/pc/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/estilos2.css">
-    <link rel="shortcut icon" href="./img/ipIcon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../img/ipIcon.png" type="image/x-icon">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
