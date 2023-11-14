5170ee73a4d070fb3493436226f600c641046ef4
abgarcia <abgarcia@tpitic.com.mx>
2023-11-14 11:51:17 -0500

    modifica la conexion a ldap por el nombre de dominio

--- php/configuraciones.class.php
+++ php/configuraciones.class.php
@@ -2,7 +2,7 @@
 $LDAPBase = 'dc=transportespitic,dc=com';
 $LDAPUserBase = 'ou=People,dc=transportespitic,dc=com';
 $LDAPGroupBase = 'ou=Groups,dc=transportespitic,dc=com';
-$ldapserver = '172.16.3.20';
+$ldapserver = 'ldap.tpitic.com.mx';
 $ldapuser      = 'cn=feria,dc=transportespitic,dc=com';  
 $ldappass     = 'sistemaspitic';
 $mysqlhost     = '192.168.100.16';
